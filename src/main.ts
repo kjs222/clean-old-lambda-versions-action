fa80f3630d2bc8eda72e510c6fc32e12bcafe71c
Kerry <kerry.sheldon@gmail.com>
2021-01-11 19:45:56 -0700

    add call to get versions associated with any alias

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,43 @@
 import * as core from '@actions/core'
-// import AWS, {Lambda} from 'aws-sdk'
+import AWS, {Lambda} from 'aws-sdk'
+
+const lambda = new Lambda({
+  apiVersion: '2015-03-31'
+})
+
+async function getAliasVersions(functionName: string): Promise<string[]> {
+  const response = await lambda
+    .listAliases({FunctionName: functionName})
+    .promise()
+
+  let versions: string[] = []
+
+  if (response.Aliases) {
+    response.Aliases.forEach(a => {
+      if (a.FunctionVersion) {
+        versions.push(a.FunctionVersion)
+      }
+
+      if (a.RoutingConfig && a.RoutingConfig.AdditionalVersionWeights) {
+        const additionalVersions = Object.keys(
+          a.RoutingConfig.AdditionalVersionWeights
+        )
+        core.debug(additionalVersions.join(','))
+        versions = [...versions, ...additionalVersions]
+      }
+    })
+  }
+  return versions
+}
 
 async function run(): Promise<void> {
   try {
     if (core.getInput('function_name')) {
       core.debug(core.getInput('function_name'))
+      const aliasVersions = await getAliasVersions(
+        core.getInput('function_name')
+      )
+      core.debug(aliasVersions.join(','))
     }
     if (core.getInput('number_to_keep')) {
       core.debug(core.getInput('number_to_keep'))
