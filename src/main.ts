dad313cd47cb23e432e28cab1ea6d8d44cb82b0a
Kerry <kerry.sheldon@gmail.com>
2021-01-11 20:24:49 -0700

    add get all versions

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,65 @@ async function getAliasVersions(functionName: string): Promise<string[]> {
   return versions
 }
 
+async function listVersions(
+  functionName: string,
+  marker: string | undefined
+): Promise<Lambda.ListVersionsByFunctionResponse> {
+  const params: Lambda.ListVersionsByFunctionRequest = {
+    FunctionName: functionName,
+    MaxItems: 2 // just for testing
+  }
+  if (marker) {
+    params.Marker = marker
+  }
+
+  return lambda.listVersionsByFunction(params).promise()
+}
+
+async function listAllVersions(functionName: string): Promise<string[]> {
+  let hasMoreVersions = true
+  let versions: Lambda.FunctionConfiguration[] = []
+  let marker
+  while (hasMoreVersions) {
+    const result: Lambda.ListVersionsByFunctionResponse = await listVersions(
+      functionName,
+      marker
+    )
+    if (!result.NextMarker) {
+      hasMoreVersions = false
+    } else {
+      core.debug('got a marker')
+      marker = result.NextMarker
+    }
+    if (result.Versions) {
+      versions = [...versions, ...result.Versions]
+    }
+    core.debug(versions.map(v => v.Version).join(','))
+  }
+  core.debug('got all the versions, lets see the original order')
+
+  core.debug(versions.map(v => v.LastModified).join(','))
+
+  const sorted = versions.sort((a, b) => {
+    let aLastModified = new Date()
+    let bLastModified = new Date()
+    if (a.LastModified) {
+      aLastModified = new Date(a.LastModified)
+    }
+    if (b.LastModified) {
+      bLastModified = new Date(b.LastModified)
+    }
+    return aLastModified.getTime() - bLastModified.getTime()
+  })
+  core.debug('got em sorted, lets see it')
+
+  core.debug(sorted.map(v => v.Version).join(','))
+  core.debug(versions.map(v => v.LastModified).join(','))
+
+  const sortedVersionNumbers = sorted.map(v => v.Version)
+  return sortedVersionNumbers.filter(v => v !== undefined) as string[]
+}
+
 async function run(): Promise<void> {
   try {
     if (core.getInput('function_name')) {
@@ -38,10 +97,12 @@ async function run(): Promise<void> {
         core.getInput('function_name')
       )
       core.debug(aliasVersions.join(','))
+      const allVersions = await listAllVersions(core.getInput('function_name'))
+      core.debug(allVersions.join(','))
     }
-    if (core.getInput('number_to_keep')) {
-      core.debug(core.getInput('number_to_keep'))
-    }
+    // if (core.getInput('number_to_keep')) {
+    //   core.debug(core.getInput('number_to_keep'))
+    // }
   } catch (error) {
     core.setFailed(error.message)
   }
